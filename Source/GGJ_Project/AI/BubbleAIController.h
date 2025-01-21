4a3f7546ea605f6b5da67a225f4ca16e5075cc07
IGOR5143515 <122176584+IGOR5143515@users.noreply.github.com>
2025-01-21 12:12:00 +0200

    Added Controller , AICharacter

---
+++ Source/GGJ_Project/AI/BubbleAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BubbleAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ_PROJECT_API ABubbleAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
