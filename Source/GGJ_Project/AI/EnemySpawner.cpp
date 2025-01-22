9320b0fae6c468ed3bac6cb46a717bed54f98271
Alina Abaieva <alina.abaieva@gmail.com>
2025-01-22 16:59:25 +0200

    created EnemySpawner actor

---
+++ Source/GGJ_Project/AI/EnemySpawner.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemySpawner.h"
+
+
+AEnemySpawner::AEnemySpawner()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AEnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AEnemySpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/GGJ_Project/AI/EnemySpawner.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemySpawner.generated.h"
+
+UCLASS()
+class GGJ_PROJECT_API AEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEnemySpawner();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
