fa4f5c44daecf711964b6805dd60ba7bf27c21a7
Alina Abaieva <alina.abaieva@gmail.com>
2025-01-25 22:14:28 +0200

    fixed Melee Booster

--- Source/GGJ_Project/Pickups/BoostSpawner_Lucky.cpp
+++ Source/GGJ_Project/Pickups/BoostSpawner_Lucky.cpp
@@ -35,54 +35,59 @@ void ABoostSpawner_Lucky::OnTimeToSpawn()
 		SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
 		// Randomize booster
-		FMath::RandInit(GetWorld()->GetTimeSeconds());
-		int Randomizer = FMath::RandRange(1, 4);
-		AActor* BoosterSpawn;
-		switch (Randomizer)
+		const int Randomizer = FMath::RandRange(1, 4);
+		if(GEngine)
 		{
-		case 1:
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, FString::Printf(TEXT("Spawner randomized %i"), Randomizer));
+		}
+		AActor* BoosterSpawn;
+		if (Randomizer == 1)
+		{
 			BoosterSpawn = GetWorld()->SpawnActor<AActor>(BoostHealth, GetActorLocation(), GetActorRotation(), SpawnParameters);
 			PointLight->SetLightColor(FColor::Green);
 			if(GEngine)
 			{
 				GEngine->AddOnScreenDebugMessage(10, 10.f, FColor::Orange, "Spawned pickups on time.");
 			}
-			break;
-		case 2:
+		}
+		else if (Randomizer == 2)
+		{
 			BoosterSpawn = GetWorld()->SpawnActor<AActor>(BoostSpeed, GetActorLocation(), GetActorRotation(), SpawnParameters);
 			PointLight->SetLightColor(FColor::Yellow);
 			if(GEngine)
 			{
 				GEngine->AddOnScreenDebugMessage(10, 10.f, FColor::Orange, "Spawned pickups on time.");
 			}
-			break;
-		case 3:
+		}
+		else if (Randomizer == 3)
+		{
 			BoosterSpawn = GetWorld()->SpawnActor<AActor>(BoostMelee, GetActorLocation(), GetActorRotation(), SpawnParameters);
 			PointLight->SetLightColor(FColor::Red);
 			if(GEngine)
 			{
 				GEngine->AddOnScreenDebugMessage(10, 10.f, FColor::Orange, "Spawned pickups on time.");
 			}
-			break;
-		case 4:
+		}
+		else if (Randomizer == 4)
+		{
 			BoosterSpawn = GetWorld()->SpawnActor<AActor>(BoostRanged, GetActorLocation(), GetActorRotation(), SpawnParameters);
 			PointLight->SetLightColor(FColor::Blue);
 			if(GEngine)
 			{
 				GEngine->AddOnScreenDebugMessage(10, 10.f, FColor::Orange, "Spawned pickups on time.");
 			}
-			break;
-		default:
+		}
+		else
+		{
 			PointLight->LightColor = FColor::Purple;
 			if(GEngine)
 			{
 				GEngine->AddOnScreenDebugMessage(10, 10.f, FColor::Orange, "Failed to spawn pickups on time.");
 			}
-			break;
 		}
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Booster types are missing."));
+		UE_LOG(LogTemp, Error, TEXT("Booster types are missing."));
 	}
 }
